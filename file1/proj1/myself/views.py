ceabbb6fa1f7e762a055b6113dbad606f22a319a
Vinuthna123 <60320769+Vinuthna123@users.noreply.github.com>
2020-08-19 00:18:52 +0530

    Update views.py

--- file1/proj1/myself/views.py
+++ file1/proj1/myself/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import HttpResponse
 import operator
 def home(requests):
     return HttpResponse('<h1> This is first page</h1>')
+def about(requests):
+    return HttpResponse('<h1> Hi, I am Vinuthna</h1>')
+def hobbies(requests):
+    return HttpResponse('<h1> Reading books, playing shuttle, Sketching</h1>)
